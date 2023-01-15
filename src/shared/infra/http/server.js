0393570ca03c04150ccb01d19f582085d0fe0755
Nilton F <jnvlfilho@hotmail.com>
2023-01-15 17:33:56 -0300

    Installation and configuration of Swagger in the application for documentation

--- src/shared/infra/http/server.js
+++ src/shared/infra/http/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const swaggerUi = require("swagger-ui-express");
 const routes = require('./routes');
+const swaggerFile = require('../../../swagger.json');
 
 const port = process.env.PORT ? process.env.PORT : 3000
 
@@ -8,6 +10,7 @@ const server = express();
 
 server.use(cors());
 server.use(express.json());
+server.use('/docs', swaggerUi.serve,swaggerUi.setup(swaggerFile));
 server.use(routes);
 
 server.listen(port, () => {
